b6b275474e6b32a55c9f6ff1fe9a9eeb1e65cc51
yy1124218793 <1124218793@qq.com>
2020-08-25 09:25:08 +0800

    提交test

---
+++ src/test/java/com/test/Test01.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * <p>Title: Test01</p>  
+ * <p>Description: </p> 
+ * @author liuqi
+ * @date 2020年8月25日
+ */
+public class Test01 {
+	//提
+	public static void main(String[] args) {
+		System.out.println("git");
+	}
+
+}
